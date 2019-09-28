d988431a85d92466044a505a7e5256fd6d588eb8
John Guest <jcguest90@gmail.com>
2019-09-28 17:05:21 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,9 +2,13 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
+<<<<<<< HEAD
 def go 
   "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
 end
+=======
+
+>>>>>>> d356ca4952b0ad79817b5bced67a15a73e7cf802
 
 
 end
